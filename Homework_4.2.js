bd015e20e8792ed8cf4b3c1112827542b78e8d55
Ihnat13 <ignatfomich7@gmail.com>
2023-12-08 19:48:39 +0200

    Fix bags

--- Homework_4.2.js
+++ Homework_4.2.js
@@ -8,13 +8,14 @@ while(valueUSD <= 100) {
 
 //Юзер вводить число. З'ясувати, чи є воно простим. Простим називається ціле число, більше ніж 1, яке не має інших дільників, 
 //крім 1 і себе (1 не є простим).
-const userInput = prompt(`Enter hole number`);
+function checkPrimeNum () {
+    const userInput = prompt(`Enter prime number`);
 
 if (
     userInput !== null &&
     !isNaN(userInput) &&
     userInput !== '0' &&
-    userInput !== '' &&
+    userInput.trim() !== '' &&
     Number.isInteger(+userInput)
     ) {
     let i = 2;
@@ -32,15 +33,48 @@ if (
     if (isPrime) {
         alert(`${userInput} is a prime number`);
     }
+} else if (userInput == null) {
+    alert(`As you wish`)
+}else {
+    alert(`Invalid number`);
+    checkPrimeNum();
 }
+}
+checkPrimeNum();
 
 //Юзер вводить число. Визначити, чи можна одержати це число шляхом зведення числа 3 в деякий цілий ступінь. 
 //Наприклад, числа 9, 81 можна отримати, бо це 3^2 і 3^4, а 13 - не можна.
-const userNumber = prompt(`Enter number`);
-const userValue = Math.abs(parseInt(userNumber, 10)).toString(); 
 
-if(userNumber !== null && !isNaN(userValue) && userNumber !== `0` && userValue !== `` && Number.isInteger(+userValue / 3 / 3)? true : false ) {
-    alert(`This number CAN be 3^*`);
+
+function checkNum() {
+const userNumber = prompt(`Enter num`);
+let userValue = Math.abs(parseInt(userNumber, 10)).toString().trim(); 
+if (
+    userNumber !== null &&
+    !isNaN(userValue) &&
+    userValue.trim() !== `0` &&
+    userValue !== `` &&
+    Number.isInteger(+userValue)
+) {
+    if (Number.isInteger(+userValue / 3)) {
+        while (+userValue % 3 === 0) {
+            userValue /= 3;
+            if (userValue === 1) {
+                alert(`This number CAN be 3^*`);
+                break;
+            } else if (userValue % 3 !== 0) {
+                alert(`This number CANT be 3^*`);
+                break;
+            }
+        }
+    } else {
+        alert(`This number CANT be 3^*`);
+    }
+} else if(userNumber === null){
+    alert(`As you wish`)
 } else {
-    alert(`This number CANT be 3^*`);
+    alert(`Invalid number`);
+    checkNum();
 }
+}
+checkNum();
